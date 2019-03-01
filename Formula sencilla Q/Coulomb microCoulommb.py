2c86dbbfad3fd0f7f9dc642c1f608a936212d6f2
Juan David <40820898+JJuan00@users.noreply.github.com>
2019-03-01 17:23:43 -0500

    Compleja (no) Simple (ciclo)

--- Formula sencilla Q/Coulomb microCoulommb.py
+++ Formula sencilla Q/Coulomb microCoulommb.py
@@ -1,7 +1,7 @@
 from math import exp, expm1
 
 def form__coulumb():
-    contanteK = 8.99*10**9
+    constanteK = 8.99*10**9
     # microCoulomb = "e-6"
     x = float(input("Ingrese primer Q -> "))
     y = int(input("Ingrese segunda Q -> "))
@@ -10,10 +10,19 @@ def form__coulumb():
     y1 = y*10**-6
     aplicatiElecQ = ((x1) * (y1))
     z = z ** 2
-    newResult = (contanteK * aplicatiElecQ)
+    newResult = (constanteK * aplicatiElecQ)
     result = (newResult / z)
     print(result)
     
 
 if __name__ == "__main__":
-    form__coulumb()
+    try:
+        c = 0
+        numRepetition = int(input("Ingrese la cantidad de veces a calcular la fuerza -> "))
+        while True:
+            form__coulumb()
+            if c == numRepetition:
+                break
+
+    except numRepetition:
+        print("Error 24")
